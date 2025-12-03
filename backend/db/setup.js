a0d7ba1f9ca155d2116150ceeea51ba1153ffe6f
Jasmin <144851658+jazzyzoney@users.noreply.github.com>
2025-12-03 19:36:35 +0000

    more

--- backend/db/setup.js
+++ backend/db/setup.js
@@ -1,12 +1,15 @@
 import db from './connection.js';
+import bcrypt from 'bcrypt'
+
+const saltRounds = 12
 
 async function setup() {
-    try {
         await db.exec(`
             CREATE TABLE IF NOT EXISTS users (
                 id INTEGER PRIMARY KEY AUTOINCREMENT,
                 email TEXT UNIQUE,
                 password TEXT,
+                username TEXT,
                 role TEXT DEFAULT 'user'
             );
         `);
@@ -22,10 +25,29 @@ async function setup() {
             );
         `);
 
-        console.log("✅ Tables created successfully!");
-    } catch (error) {
-        console.error("❌ Error setting up database:", error);
+        //seeding
+        const admins =[
+            { name: 'Cloe', email: 'cloe@bratz.com' },
+            { name: 'Jade', email: 'jade@bratz.com' },
+            { name: 'Sasha', email: 'sasha@bratz.com' },
+            { name: 'Yasmin', email: 'yasmin@bratz.com' }
+        ]
+
+        const passwordHash = await bcrypt.hash(ADMIN_PASS, saltRounds);
+
+        for (const admin of admins) {
+            try {
+                await db.run(
+                    `INSERT INTO users (username, email, password, role) VALUES (?, ?, ?, ?)`,
+                    [admin.name, admin.email, passwordHash, 'admin']
+                );
+                console.log(`✨ Created Admin: ${admin.name}`);
+            } catch (err) {
+            // Ignore error if they already exist
+            }
+        }
+    
+        console.log("✅ Database setup complete.");
     }
-}
 
 setup();
